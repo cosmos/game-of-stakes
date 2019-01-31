47195bda479be00c1b93719ee9ebd30bee369c64
Christopher Goes <cwgoes@pluranimity.org>
2019-01-31 22:48:34 +0100

    More work on vesting accounts

--- genesisify/add-vesting.py
+++ genesisify/add-vesting.py
@@ -12,12 +12,37 @@ old = json.loads(raw)
 addrs = set([x for x in json.load(open('addrs.json'))])
 print('{} whitelisted accounts'.format(len(addrs)))
 
-# Add vesting coins to whitelisted accounts
-for account in old['app_state']['accounts']:
-  if account['address'] in addrs:
-    print('Account {} matched'.format(account['address']))
+supply_diff = 0
+
+def add_vesting_account(addr):
+  global supply_diff
+  found = [x for x in old['app_state']['accounts'] if x['address'] == addr]
+  if len(found) > 0:
+    found = found[0]
+    if found['coins'] is None:
+      st = []
+      found['coins'] = []
+    else:
+      st = [x for x in found['coins'] if x['denom'] == 'stake']
+    if len(st) > 0:
+      st = st[0]
+      st['amount'] = str(int(st['amount']) + 500000)
+    else:
+      found['coins'].append({'denom': 'stake', 'amount': '500000'})
+    found['original_vesting'] = [{'denom': 'stake', 'amount': '500000'}]
+    found['start_time'] = '1548970600'
+    found['end_time'] = '1550180200'
+    print('Address {} found'.format(addr))
+    supply_diff = supply_diff + 500000
   else:
-    print('Account {} did not match'.format(account['address']))
+    print('Address {} not found, skipping'.format(addr))
+
+# Add vesting coins to whitelisted accounts
+for addr in addrs:
+  add_vesting_account(addr)
+
+# Update supply
+old['app_state']['staking']['pool']['not_bonded_tokens'] = str(int(old['app_state']['staking']['pool']['not_bonded_tokens']) + supply_diff)
 
 new = sys.argv[2]
 json.dump(old, open(new, 'w'), indent = True)
